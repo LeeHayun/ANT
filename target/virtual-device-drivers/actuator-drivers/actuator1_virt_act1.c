dcf79f0eb779ba9a127b0a70904b564aea648bed
Hayun Lee <lhy920806@gmail.com>
2018-12-04 04:53:52 +0000

    ODROID XU4 porting

--- target/virtual-device-drivers/actuator-drivers/actuator1_virt_act1.c
+++ target/virtual-device-drivers/actuator-drivers/actuator1_virt_act1.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include <wiringPi.h>
+//#include <wiringPi.h>
 
 #define LED 5 // BCM_GPIO 24
 
 void LED_start(void *data)
 {
-	printf("LED sensor is started\n");
+	printf("[virt] LED sensor is started\n");
 }
 
 void LED_stop(void *data)
 {
-	printf("LED sensor is terminated\n");
+	printf("[virt] LED sensor is terminated\n");
 }
 
 char* LED_set(void *data)
 {
+  /*
   if(wiringPiSetup() == -1)
     return 1;
 
   pinMode(LED, OUTPUT);
 
   digitalWrite(LED, 1);
+  */
+  printf("[virt] LED set\n");
 
   return 0;
 }
